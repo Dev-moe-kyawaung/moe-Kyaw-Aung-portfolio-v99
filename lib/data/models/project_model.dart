4babde6e037c0978ba36b43c70a0f2cc28efa187
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-16 23:59:29 +0630

    Create project_model.dart

---
+++ lib/data/models/project_model.dart
@@ -0,0 +1,9 @@
+class ProjectModel {
+  final int id;
+  final String title;
+  final String image;
+  final String github;
+  final String tech;
+  final String description;
+  ProjectModel({required this.id, required this.title, required this.image, required this.github, required this.tech, required this.description});
+}
