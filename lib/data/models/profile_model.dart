4276d6e3a9ae010f3844753db8781f0cef861557
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-16 23:58:53 +0630

    Create profile_model.dart

---
+++ lib/data/models/profile_model.dart
@@ -0,0 +1,9 @@
+class ProfileModel {
+  final String name;
+  final String tagline;
+  final int yearsExperience;
+  final String location;
+  final String languages;
+  final String profileImage;
+  ProfileModel({required this.name, required this.tagline, required this.yearsExperience, required this.location, required this.languages, required this.profileImage});
+}
