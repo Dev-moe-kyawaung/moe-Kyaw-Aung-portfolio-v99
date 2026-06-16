5271441af610d0423d4a7cd00204ae60aca75321
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-16 23:58:21 +0630

    Create achievement_model.dart

---
+++ lib/data/models/achievement_model.dart
@@ -0,0 +1,7 @@
+class AchievementModel {
+  final int id;
+  final String title;
+  final String value;
+  final String unit;
+  AchievementModel({required this.id, required this.title, required this.value, required this.unit});
+}
