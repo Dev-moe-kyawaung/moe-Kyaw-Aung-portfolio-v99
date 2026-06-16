eb891525eb046e0e46464283a1041132443da3ae
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-17 00:00:05 +0630

    Create service_model.dart

---
+++ lib/data/models/service_model.dart
@@ -0,0 +1,7 @@
+class ServiceModel {
+  final int id;
+  final String title;
+  final String price;
+  final String description;
+  ServiceModel({required this.id, required this.title, required this.price, required this.description});
+}
