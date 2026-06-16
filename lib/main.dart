a9a1c9f9b56ecb02fbe5556d467ff3a88e933d36
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-17 00:19:03 +0630

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:three_dart/three_dart.dart' as three;
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'app/app.dart';
 
 void main() async {
@@ -11,23 +11,12 @@ void main() async {
 
 class MoeKyawAungPortfolioV99 extends StatelessWidget {
   const MoeKyawAungPortfolioV99({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Moe Kyaw Aung - Senior Android Developer',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF00D9A5),
-        scaffoldBackgroundColor: const Color(0xFF0A0A0A),
-      ),
-      locale: const Locale('en'),
-      supportedLocales: const [Locale('en'), Locale('my')],
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
+      theme: ThemeData.dark().copyWith(primaryColor: const Color(0xFF00D9A5)),
       home: const PortfolioShellPage(),
     );
   }
